e37a7520b71c761b88dd36860af91f413395c66d
vikram1160 <vgeditor12@gmail.com>
2023-12-20 07:07:51 -0800

    h1 tag add

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,6 +6,7 @@ function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
+                <h1 className="navbar-brand text-danger fw-bold">Vikram</h1>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
