94c465b62d17ec4dba8c88b531ddbf0baf4f0c3c
vikram1160 <vgeditor12@gmail.com>
2023-12-23 22:44:35 -0800

    add toggle

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,44 +1,98 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Assuming you are using React Router
 import './NavBar.css'
-import { Link } from 'react-router-dom'
 
-function NavBar() {
-     const [isMobile, setIsMobile]=useState(false)
-    function ToggleEvent(){
-        alert("working my website")
-    }
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  const toggleNavbar = () => {
+    setShowNavbar(!showNavbar);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand text-danger fw-bold" href="/">Vikram</a>
+
+      <button
+        onClick={toggleNavbar}
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        {showNavbar ? '' : ''}<span  className="navbar-toggler-icon" ></span>
+      </button>
+
+      <div className={`collapse navbar-collapse ${showNavbar ? 'show' : ''}`} id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/education">
+              Education
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/technology">
+              Technology
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/projects">
+              Projects
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+
+
+export default Navbar;
+
+
+// function NavBar() {
+//      const [show, toggleShow]=useState(false)
   
-    return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand text-danger fw-bold" href="/">Vikram</a>
+//     return (
+//         <nav className="navbar navbar-expand-lg bg-body-tertiary">
+//             <div className="container-fluid">
+//                 <a className="navbar-brand text-danger fw-bold" href="/">Vikram</a>
 
-                <button onClick={ToggleEvent} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" ></span> 
-                </button>
+//                 <button onClick={() =>toggleShow(!show)} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//                 {show ? "Hide" : "Show"}
+//                     <span className="navbar-toggler-icon" ></span> 
+//                 </button>
                 
-                <div className="collapse navbar-collapse" >
-                    <ul className={"navbar-nav" } me-auto mb-2 mb-lg-0>
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="education">Education</Link>
-                        </li>
+//                 <div className="collapse navbar-collapse ${show ? 'show' : ''}`" >
+//                     <ul className={"navbar-nav" } me-auto mb-2 mb-lg-0>
+//                         <li className="nav-item">
+//                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+//                         </li>
+//                         <li className="nav-item">
+//                             <Link className="nav-link" to="education">Education</Link>
+//                         </li>
                         
-                        <li className="nav-item">
-                            <Link className="nav-link" to="technology">Technology</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="projects">Projects</Link>
-                        </li>
-                    </ul>
+//                         <li className="nav-item">
+//                             <Link className="nav-link" to="technology">Technology</Link>
+//                         </li>
+//                         <li className="nav-item">
+//                             <Link className="nav-link" to="projects">Projects</Link>
+//                         </li>
+//                     </ul>
                 
-                </div>
+//                 </div>
 
-            </div>
-        </nav>
-    )
-}
+//             </div>
+//         </nav>
+//     )
+// }
 
-export default NavBar
