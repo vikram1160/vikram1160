64fef2d200eb9dd8b4b84a0e6d3e9ffa62961323
vikram1160 <vgeditor12@gmail.com>
2023-12-24 23:19:40 -0800

    color

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,28 +6,15 @@ import vikram from '../assets/vikram.jpg.jpg'
 const AboutMe = () => {
   return (
     <>
-   <div id="carouselExampleFade" class="carousel slide carousel-fade">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={vikram} class="d-block  " alt="img"/>
+   <div className="d-flex">
+      <img src={vikram} className="d-block  " alt="..."/>
+      <div className='mx-5' >
+    <h3 className='md'>  I  Am  <span className='vikram'>Vikram</span></h3>
+    <h5 className='sam'>I Am From Maharashtra, Dist <span className='pm'>Sambhaji Nagar</span>,</h5>
     </div>
-    <div class="carousel-item">
-      <img src={vikram}class="d-block " alt="..."/>
     </div>
-    <div class="carousel-item">
-      <img src={vikram} class="d-block  " alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-    </>
+    
+        </>
   )
 }
 
