01523ec983ad3b728d4cae6b1d9bc5542458efd2
vikram1160 <vgeditor12@gmail.com>
2023-12-24 23:58:29 -0800

    color

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,8 +9,15 @@ const AboutMe = () => {
    <div className="">
       <img src={vikram} className="d-block  " alt="..."/>
       <div className='mx-5' >
-    <h3 className='md'>  I  Am  <span className='vikram'>Vikram</span></h3>
-    <h5 className='sam'>I Am From Maharashtra, Dist <span className='pm'>Sambhaji Nagar</span>,</h5>
+    <h3 className='md'>*I Am  <span className='vikram'>Vikram</span></h3>
+    <h5 className='sam'>*I Am From Maharashtra, Dist <span className='pm'>Sambhaji Nagar</span>,</h5>
+    <h5>*Front End Developer, And UI UX Designer</h5>
+    <h3><span className='class'>*Framework </span>Angular,
+      React      <br/>
+      <h3><span className='class1'>*Database </span>MySql</h3>
+      <h3><span className='class2'>*Backend </span>Basic Java</h3>
+      
+    </h3>
     </div>
     </div>
     
